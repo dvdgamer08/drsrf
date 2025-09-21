37c9cc9d7f938bdd5640a00ee27cf8194bc69292
dvdgamer08 <134729003+dvdgamer08@users.noreply.github.com>
2025-09-21 15:50:57 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,16 +1,3 @@
--- helloworld.lua
-
--- Get config parameters passed via server
-local params = script_params or {}  -- depending on exact API naming
-local foo = params.PARAM_FOO or "default foo"
-local bar = params.PARAM_BAR or "default bar"
-
--- Example: when session starts, display a chat message
 function ac.onSessionStart()
-    ac.msg("Hello from online script! foo=" .. tostring(foo) .. ", bar=" .. tostring(bar))
-end
-
--- Optionally do something per frame
-function ac.onUpdate(dt)
-    -- nothing for now
+    ac.log("Hello from online script!")
 end
